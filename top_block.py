3c9eb1d1af675c863a8f17c6b5839887e3163a70
Optics lab PC <xnghatsane@gmail.com>
2019-07-17 15:17:44 +0200

    Works for not extremely tiny files.

--- top_block.py
+++ top_block.py
@@ -3,8 +3,10 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# GNU Radio version: 3.7.13.5
+# Generated: Wed Jul 17 15:16:38 2019
 ##################################################
+
+from distutils.version import StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -16,7 +18,7 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from PyQt4 import Qt
+from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
@@ -25,7 +27,6 @@ from gnuradio import gr
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
-import pmt
 import sys
 from gnuradio import qtgui
 
@@ -54,73 +55,61 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_layout.addLayout(self.top_grid_layout)
 
         self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
+        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+            self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        else:
+            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Variables
         ##################################################
-        self.scale = scale = 1
         self.PackRate = PackRate = 8
-        self.FrameBits = FrameBits = 8
-        self.samp_rate = samp_rate = 512e3
+        self.FrameBits = FrameBits = 2048
+        self.scale = scale = 1
+        self.samp_rate = samp_rate = 150e6
 
 
-        self.encoder = encoder = fec.cc_encoder_make(FrameBits*PackRate, 7, 2, ([79,109]), 0, fec.CC_TERMINATED, True)
+        self.encoder = encoder = fec.cc_encoder_make(FrameBits*PackRate, 7, 2, ([79,109]), 0, fec.CC_STREAMING, True)
 
 
 
-        self.decoder = decoder = fec.cc_decoder.make(FrameBits*PackRate*scale, 7, 2, ([79,109]), 0, 0, fec.CC_TERMINATED, True)
+        self.decoder = decoder = fec.cc_decoder.make(FrameBits*PackRate, 7, 2, ([79,109]), 0, -1, fec.CC_STREAMING, True)
 
         self.UnpackRate = UnpackRate = 8
 
         ##################################################
         # Blocks
         ##################################################
-        self.fec_extended_encoder_0 = fec.extended_encoder(encoder_obj_list=encoder, threading='capillary', puncpat='11')
-        self.fec_extended_decoder_0 = fec.extended_decoder(decoder_obj_list=decoder, threading='capillary', ann=None, puncpat='11', integration_period=10000)
+        self.fec_extended_tagged_encoder_0 = fec.extended_tagged_encoder(encoder_obj_list=encoder, puncpat='11', lentagname='packet_len', mtu=1500)
+        self.fec_extended_tagged_decoder_1 = self.fec_extended_tagged_decoder_1 = fec_extended_tagged_decoder_1 = fec.extended_tagged_decoder(decoder_obj_list=decoder, ann=None, puncpat='11', integration_period=10000, lentagname='packet_len', mtu=1500)
         self.digital_map_bb_0 = digital.map_bb((-1,1))
-        self.blocks_unpacked_to_packed_xx_0 = blocks.unpacked_to_packed_bb(1, gr.GR_MSB_FIRST)
+        self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(8)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
-        self.blocks_packed_to_unpacked_xx_0 = blocks.packed_to_unpacked_bb(1, gr.GR_MSB_FIRST)
-        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(8)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, 'C:\\Users\\XongileN\\Google Drive\\2019\\Semester2\\ELEN40024012 lab project\\Lab-Project\\TestInput.txt', False)
-        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
-        self.blocks_file_sink_0_0_0_0_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\XongileN\\Google Drive\\2019\\Semester2\\ELEN40024012 lab project\\Lab-Project\\TestInputTXPacked.txt', False)
-        self.blocks_file_sink_0_0_0_0_0.set_unbuffered(False)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\XongileN\\Google Drive\\2019\\Semester2\\ELEN40024012 lab project\\Lab-Project\\TestOutput.txt', False)
-        self.blocks_file_sink_0_0.set_unbuffered(False)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\XongileN\\Google Drive\\2019\\Semester2\\ELEN40024012 lab project\\Lab-Project\\TestOutputFEC.txt', False)
+        self.blocks_stream_to_tagged_stream_0 = blocks.stream_to_tagged_stream(gr.sizeof_char, 1, UnpackRate, "packet_len")
+        self.blocks_pack_k_bits_bb_0_0 = blocks.pack_k_bits_bb(8)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/xongile/Lab-Project/TestInput.txt', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/xongile/Lab-Project/TestOutputFEC.txt', False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_char_to_float_0 = blocks.char_to_float(1, 1)
-
-
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_char_to_float_0, 0), (self.fec_extended_decoder_0, 0))
+        self.connect((self.blocks_char_to_float_0, 0), (self.fec_extended_tagged_decoder_1, 0))
         self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.blocks_file_sink_0_0_0_0_0, 0))
-        self.connect((self.blocks_packed_to_unpacked_xx_0, 0), (self.fec_extended_encoder_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_packed_to_unpacked_xx_0, 0))
-        self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_pack_k_bits_bb_0_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect((self.blocks_stream_to_tagged_stream_0, 0), (self.fec_extended_tagged_encoder_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_stream_to_tagged_stream_0, 0))
         self.connect((self.digital_map_bb_0, 0), (self.blocks_char_to_float_0, 0))
-        self.connect((self.fec_extended_decoder_0, 0), (self.blocks_unpacked_to_packed_xx_0, 0))
-        self.connect((self.fec_extended_encoder_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
-        self.connect((self.fec_extended_encoder_0, 0), (self.digital_map_bb_0, 0))
+        self.connect((self.fec_extended_tagged_decoder_1, 0), (self.blocks_pack_k_bits_bb_0_0, 0))
+        self.connect((self.fec_extended_tagged_encoder_0, 0), (self.digital_map_bb_0, 0))
 
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "top_block")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
-
-    def get_scale(self):
-        return self.scale
-
-    def set_scale(self, scale):
-        self.scale = scale
 
     def get_PackRate(self):
         return self.PackRate
@@ -133,6 +122,12 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_FrameBits(self, FrameBits):
         self.FrameBits = FrameBits
+
+    def get_scale(self):
+        return self.scale
+
+    def set_scale(self, scale):
+        self.scale = scale
 
     def get_samp_rate(self):
         return self.samp_rate
@@ -158,12 +153,13 @@ class top_block(gr.top_block, Qt.QWidget):
 
     def set_UnpackRate(self, UnpackRate):
         self.UnpackRate = UnpackRate
+        self.blocks_stream_to_tagged_stream_0.set_packet_len(self.UnpackRate)
+        self.blocks_stream_to_tagged_stream_0.set_packet_len_pmt(self.UnpackRate)
 
 
 def main(top_block_cls=top_block, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
@@ -175,7 +171,7 @@ def main(top_block_cls=top_block, options=None):
     def quitting():
         tb.stop()
         tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 
