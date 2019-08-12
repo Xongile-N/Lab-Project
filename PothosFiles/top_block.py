08110aa180cc85143302ba86f903479b8e21ff23
Optics lab PC <xnghatsane@gmail.com>
2019-08-13 01:16:13 +0200

    Just wnat this to be done tbh

--- PothosFiles/top_block.py
+++ PothosFiles/top_block.py
@@ -3,8 +3,10 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# GNU Radio version: 3.7.13.5
+# Generated: Tue Aug 13 00:57:28 2019
 ##################################################
+
+from distutils.version import StrictVersion
 
 if __name__ == '__main__':
     import ctypes
@@ -16,7 +18,8 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
-from PyQt4 import Qt
+from PyQt5 import Qt
+from PyQt5 import Qt, QtCore
 from gnuradio import blocks
 from gnuradio import digital
 from gnuradio import eng_notation
@@ -25,6 +28,7 @@ from gnuradio import qtgui
 from gnuradio.eng_option import eng_option
 from gnuradio.filter import firdes
 from optparse import OptionParser
+import MyQPSK
 import sip
 import sys
 from gnuradio import qtgui
@@ -54,8 +58,11 @@ class top_block(gr.top_block, Qt.QWidget):
         self.top_layout.addLayout(self.top_grid_layout)
 
         self.settings = Qt.QSettings("GNU Radio", "top_block")
-        self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
+        if StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
+            self.restoreGeometry(self.settings.value("geometry").toByteArray())
+        else:
+            self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
 
         ##################################################
         # Variables
@@ -87,7 +94,7 @@ class top_block(gr.top_block, Qt.QWidget):
         	1 #number of inputs
         )
         self.qtgui_time_sink_x_1.set_update_time(0.10)
-        self.qtgui_time_sink_x_1.set_y_axis(-255, 255)
+        self.qtgui_time_sink_x_1.set_y_axis(-1, 1)
 
         self.qtgui_time_sink_x_1.set_y_label('Amplitude', "")
 
@@ -127,7 +134,7 @@ class top_block(gr.top_block, Qt.QWidget):
             self.qtgui_time_sink_x_1.set_line_alpha(i, alphas[i])
 
         self._qtgui_time_sink_x_1_win = sip.wrapinstance(self.qtgui_time_sink_x_1.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_time_sink_x_1_win)
+        self.top_layout.addWidget(self._qtgui_time_sink_x_1_win)
         self.qtgui_const_sink_x_1_0 = qtgui.const_sink_c(
         	1024, #size
         	"DQPSK Block", #name
@@ -168,7 +175,7 @@ class top_block(gr.top_block, Qt.QWidget):
             self.qtgui_const_sink_x_1_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_const_sink_x_1_0_win = sip.wrapinstance(self.qtgui_const_sink_x_1_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_1_0_win)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_1_0_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
         	1024, #size
         	"", #name
@@ -209,13 +216,13 @@ class top_block(gr.top_block, Qt.QWidget):
             self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
 
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
-        self.top_grid_layout.addWidget(self._qtgui_const_sink_x_0_win)
+        self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
         self.digital_pfb_clock_sync_xxx_0 = digital.pfb_clock_sync_ccf(sps, 6.28/100, (rx_rrc_taps), 32, 16, 1.5, 2)
+        self.digital_map_bb_0 = digital.map_bb(((0,1,3,2)))
         self.digital_diff_decoder_bb_0 = digital.diff_decoder_bb(4)
         self.digital_costas_loop_cc_0 = digital.costas_loop_cc(6.28/100, 4, False)
-        self.digital_constellation_soft_decoder_cf_0 = digital.constellation_soft_decoder_cf(const)
         self.digital_constellation_modulator_0 = digital.generic_mod(
-          constellation=const,
+          constellation=qpsk,
           differential=True,
           samples_per_symbol=sps,
           pre_diff_code=True,
@@ -224,35 +231,31 @@ class top_block(gr.top_block, Qt.QWidget):
           log=False,
           )
         self.digital_cma_equalizer_cc_0 = digital.cma_equalizer_cc(15, 1, 0.01, 2)
-        self.digital_binary_slicer_fb_0 = digital.binary_slicer_fb()
         self.blocks_vector_source_x_0_0 = blocks.vector_source_b(range(256), True, 1, [])
         self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(2)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
         self.blocks_pack_k_bits_bb_1 = blocks.pack_k_bits_bb(8)
-        self.blocks_pack_k_bits_bb_0 = blocks.pack_k_bits_bb(2)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, 'C:\\Users\\XongileN\\GoogleDrive\\2019\\Semester2\\ELEN40024012_lab_project\\Lab-Project\\TestSinks\\QPSKSimpleNORP.dat', False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, '/home/xongile/Lab-Project/TestSinks/QPSKSimple.dat', False)
         self.blocks_file_sink_0.set_unbuffered(False)
         self.blocks_char_to_float_0_0 = blocks.char_to_float(1, 1)
-
-
+        self.MyQPSK_my_qpsk_demod_cb_0 = MyQPSK.my_qpsk_demod_cb(True)
 
         ##################################################
         # Connections
         ##################################################
+        self.connect((self.MyQPSK_my_qpsk_demod_cb_0, 0), (self.digital_map_bb_0, 0))
         self.connect((self.blocks_char_to_float_0_0, 0), (self.qtgui_time_sink_x_1, 0))
-        self.connect((self.blocks_pack_k_bits_bb_0, 0), (self.digital_diff_decoder_bb_0, 0))
-        self.connect((self.blocks_pack_k_bits_bb_1, 0), (self.blocks_char_to_float_0_0, 0))
         self.connect((self.blocks_pack_k_bits_bb_1, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_throttle_0, 0), (self.digital_constellation_modulator_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_char_to_float_0_0, 0))
         self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_pack_k_bits_bb_1, 0))
         self.connect((self.blocks_vector_source_x_0_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.digital_binary_slicer_fb_0, 0), (self.blocks_pack_k_bits_bb_0, 0))
         self.connect((self.digital_cma_equalizer_cc_0, 0), (self.digital_costas_loop_cc_0, 0))
         self.connect((self.digital_constellation_modulator_0, 0), (self.digital_pfb_clock_sync_xxx_0, 0))
-        self.connect((self.digital_constellation_soft_decoder_cf_0, 0), (self.digital_binary_slicer_fb_0, 0))
-        self.connect((self.digital_costas_loop_cc_0, 0), (self.digital_constellation_soft_decoder_cf_0, 0))
+        self.connect((self.digital_costas_loop_cc_0, 0), (self.MyQPSK_my_qpsk_demod_cb_0, 0))
         self.connect((self.digital_costas_loop_cc_0, 0), (self.qtgui_const_sink_x_1_0, 0))
         self.connect((self.digital_diff_decoder_bb_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.digital_map_bb_0, 0), (self.digital_diff_decoder_bb_0, 0))
         self.connect((self.digital_pfb_clock_sync_xxx_0, 0), (self.digital_cma_equalizer_cc_0, 0))
 
     def closeEvent(self, event):
@@ -326,8 +329,7 @@ class top_block(gr.top_block, Qt.QWidget):
 
 def main(top_block_cls=top_block, options=None):
 
-    from distutils.version import StrictVersion
-    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+    if StrictVersion("4.5.0") <= StrictVersion(Qt.qVersion()) < StrictVersion("5.0.0"):
         style = gr.prefs().get_string('qtgui', 'style', 'raster')
         Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
@@ -339,7 +341,7 @@ def main(top_block_cls=top_block, options=None):
     def quitting():
         tb.stop()
         tb.wait()
-    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.aboutToQuit.connect(quitting)
     qapp.exec_()
 
 
